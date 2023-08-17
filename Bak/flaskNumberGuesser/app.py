a2cfd6fb28c624c956342bba9d5d680b82bf3853
RCHennig <luis.hennig@realcore.de>
2023-08-17 11:35:46 +0200

    add db

--- Bak/flaskNumberGuesser/app.py
+++ Bak/flaskNumberGuesser/app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session
 import random
-import time
+import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
-TIME_LIMIT = 10  # Time limit for each guess in seconds
+
+# Set up the MySQL connection
+db_connection = mysql.connector.connect(
+    host="your_host",
+    user="your_user",
+    password="your_password",
+    database="your_database"
+)
+db_cursor = db_connection.cursor()
 
 def start_new_round():
     session["target_number"] = random.randint(1, 100)
-    session["start_time"] = time.time()
+
+def save_score(player, score):
+    sql = "INSERT INTO scores (player, score) VALUES (%s, %s)"
+    values = (player, score)
+    db_cursor.execute(sql, values)
+    db_connection.commit()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if "score" not in session:
         session["score"] = 0
-    if "target_number" not in session or time.time() - session["start_time"] > TIME_LIMIT:
+    if "target_number" not in session:
         start_new_round()
 
     if request.method == "POST":
         guess = int(request.form["guess"])
         if guess == session["target_number"]:
             session["message"] = "Congratulations! You guessed the correct number."
+            player_name = "Player"  # You can customize this to take player's name as input
+            save_score(player_name, session["score"])
             session["score"] += 10
             start_new_round()
         elif guess < session["target_number"]:
@@ -31,6 +46,13 @@ def index():
             session["score"] -= 1
 
     return render_template("index.html", score=session["score"], message=session.get("message"))
+
+@app.route("/scores")
+def scores():
+    sql = "SELECT player, score FROM scores ORDER BY score DESC"
+    db_cursor.execute(sql)
+    scores = db_cursor.fetchall()
+    return render_template("scores.html", scores=scores)
 
 if __name__ == "__main__":
     app.run(debug=True)
